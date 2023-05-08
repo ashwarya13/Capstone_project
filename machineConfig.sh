e414b04d36ad94fd380aec69bf764774106137ef
ashwarya13 <ashwaryavikram13@gmail.com>
2023-05-08 13:20:14 +0530

    Update machineConfig.sh

--- machineConfig.sh
+++ machineConfig.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 apt update -y
 snap install terraform --classic
-apt install ansible
-apt install awscli
+apt -y install ansible
+apt -y install awscli
 ansible-galaxy collection install amazon.aws
 git clone https://github.com/ashwarya13/Capstone_project.git
 git clone https://github.com/ashwarya13/kube-ansible.git
